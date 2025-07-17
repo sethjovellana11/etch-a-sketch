95a90df4152ef0ffb3469732de6061ed0a749a48
sethjovellana11 <201828872+sethjovellana11@users.noreply.github.com>
2025-07-18 07:24:27 +0800

    Implemented the script and frontend of the app
    Followed the specifications. Didn't manage to implement the opacity and eraser. The sketch would reset when another color brush
    is picked.

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const gridContainer = document.querySelector('#container');
 const input = document.querySelector('input');
 const generateBtn = document.querySelector('#generateBtn');
 const resetBtn = document.querySelector('#resetBtn');
+const blackBtn = document.querySelector('#blackBtn');
+const rgbBtn = document.querySelector('#rgbBtn');
 
 let resetSize = 16;
+let blackSelected = true;
+let rgbSelected = false;
+let r, g, b;
 
 function createGrid(size) {
     gridContainer.innerHTML = '';
@@ -21,9 +26,18 @@ function createGrid(size) {
         gridSquare.style.flexBasis = `${gridSquareSize}%`;
         gridSquare.style.height = `${gridSquareSize}%`;
 
-        gridSquare.addEventListener('mouseenter', () => {
-            gridSquare.style.backgroundColor = 'black';
-        })
+        if (blackSelected){
+            gridSquare.addEventListener('mouseenter', (e) => {
+                gridSquare.style.backgroundColor = 'black';
+            });      
+        } else if (rgbSelected) {
+            gridSquare.addEventListener('mouseenter', (e) => {
+                r = Math.floor(Math.random() * 256);
+                g = Math.floor(Math.random() * 256);
+                b = Math.floor(Math.random() * 256);
+                gridSquare.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            });    
+        }
 
         gridContainer.append(gridSquare);
     }
@@ -32,12 +46,27 @@ function createGrid(size) {
 generateBtn.addEventListener('click', () => {
     const newSize = parseInt(input.value);
     resetSize = newSize;
-    input.value = '';
     createGrid(newSize);
 });
 
 resetBtn.addEventListener('click', () => {
     createGrid(resetSize);
+});
+
+blackBtn.addEventListener('click', () => {
+    blackSelected = true;
+    rgbSelected = false;
+    blackBtn.style.border = '10px solid orangered';
+    rgbBtn.style.border = '4px solid red';
+    resetBtn.click();
+});
+
+rgbBtn.addEventListener('click', () => {
+    rgbSelected = true;
+    blackSelected = false;
+    rgbBtn.style.border = '10px solid orangered';
+    blackBtn.style.border = 'none';
+    resetBtn.click();
 });
 
 input.addEventListener('keyup', (event) => {
@@ -55,6 +84,7 @@ window.addEventListener('keyup', (event) => {
 window.addEventListener('load', () => {
     input.value = 16;
     const initialSize = parseInt(input.value);
+    blackBtn.style.border = '10px solid orangered';
     createGrid(initialSize);
 });
 
