df22cefd57c11d0e0dd4ce988f2931bc02bad308
sethjovellana11 <201828872+sethjovellana11@users.noreply.github.com>
2025-09-20 02:21:16 +0800

    Modified script to not reset grid w color change

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const generateBtn = document.querySelector('#generateBtn');
 const resetBtn = document.querySelector('#resetBtn');
 const blackBtn = document.querySelector('#blackBtn');
 const rgbBtn = document.querySelector('#rgbBtn');
+const eraserBtn = document.querySelector('#eraserBtn');
 
 let resetSize = 16;
 let blackSelected = true;
 let rgbSelected = false;
-let r, g, b;
+let color = "black";
+let r = Math.floor(Math.random() * 256);
+let g = Math.floor(Math.random() * 256);
+let b = Math.floor(Math.random() * 256);
 
 function createGrid(size) {
     gridContainer.innerHTML = '';
@@ -26,21 +30,37 @@ function createGrid(size) {
         gridSquare.style.flexBasis = `${gridSquareSize}%`;
         gridSquare.style.height = `${gridSquareSize}%`;
 
-        if (blackSelected){
-            gridSquare.addEventListener('mouseenter', (e) => {
-                gridSquare.style.backgroundColor = 'black';
-            });      
-        } else if (rgbSelected) {
-            gridSquare.addEventListener('mouseenter', (e) => {
-                r = Math.floor(Math.random() * 256);
-                g = Math.floor(Math.random() * 256);
-                b = Math.floor(Math.random() * 256);
-                gridSquare.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-            });    
-        }
+        // if (blackSelected){
+        //     gridSquare.addEventListener('mouseenter', (e) => {
+        //         gridSquare.style.backgroundColor = 'black';
+        //     });      
+        // } else if (rgbSelected) {
+        //     gridSquare.addEventListener('mouseenter', (e) => {
+        //         r = Math.floor(Math.random() * 256);
+        //         g = Math.floor(Math.random() * 256);
+        //         b = Math.floor(Math.random() * 256);
+        //         gridSquare.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        //     });    
+        // }
+        gridSquare.addEventListener("mouseenter", colorGrid);
 
         gridContainer.append(gridSquare);
     }
+}
+
+function colorGrid() {
+    if (rgbSelected) {
+        r = Math.floor(Math.random() * 256);
+        g = Math.floor(Math.random() * 256);
+        b = Math.floor(Math.random() * 256);
+        this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
 }
 
 generateBtn.addEventListener('click', () => {
@@ -54,20 +74,30 @@ resetBtn.addEventListener('click', () => {
 });
 
 blackBtn.addEventListener('click', () => {
-    blackSelected = true;
     rgbSelected = false;
+    changeColor("black");
     blackBtn.style.border = '10px solid orangered';
     rgbBtn.style.border = '4px solid red';
-    resetBtn.click();
+    eraserBtn.style.border = 'none';
+    // resetBtn.click();
 });
 
 rgbBtn.addEventListener('click', () => {
     rgbSelected = true;
-    blackSelected = false;
     rgbBtn.style.border = '10px solid orangered';
     blackBtn.style.border = 'none';
-    resetBtn.click();
+    eraserBtn.style.border = 'none';
+    // resetBtn.click();
 });
+eraserBtn.addEventListener('click', () => {
+    rgbSelected = false;
+    changeColor("lightgreen");
+    rgbBtn.style.border = '4px solid red';
+    blackBtn.style.border = 'none';
+    eraserBtn.style.border = '10px solid orangered';
+    // resetBtn.click();
+});
+
 
 input.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
